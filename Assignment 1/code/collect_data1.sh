fee2608902add52e1e0731e290b03fdc3eb9a2af
Frédéric Gillioz <fredgillioz@web.de>
2016-11-06 02:52:24 +0100

    wip

--- Assignment 1/code/collect_data1.sh
+++ Assignment 1/code/collect_data1.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-for i in {1..500}; do
+nlist=(250, 300, 350, 400, 450, 500, 550, 600, 650, 700, 750)
+for n in "${nlist[@]}"; do
 	# Measure performance without valgrind overhead.
-	./a.out $i
-	valgrind --tool=cachegrind ./a.out $i silent 2> cachegrind.txt
+	./a.out $n
+	valgrind --tool=cachegrind ./a.out $n silent 2> cachegrind.txt
 	grep -o -P '(?<=D1  miss rate:)[^%]*' cachegrind.txt | xargs >> logfile_dgemm.txt
 	rm cachegrind.txt
 done
